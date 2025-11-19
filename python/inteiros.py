4a71a8783d0c4d7e1a138b90cb7ef3dd7df5aa87
PedroSampaio <henripedro652@gmail.com>
2025-11-18 21:53:27 -0300

    Create inteiros.py

---
+++ python/inteiros.py
@@ -0,0 +1,10 @@
+print(10 + 10)
+
+# flutuantes - números com casas decimais (abaixo)
+""" Comentarios grandes..."""
+
+print(10.5 + 10.5)
+
+# boleanos - True ou False (abaixo)
+print(True)
+print(False)
