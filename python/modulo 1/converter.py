32af2309291928fbea71a2425682430f359f0e9e
PedroSampaio <henripedro652@gmail.com>
2025-11-19 21:40:20 -0300

    Update converter.py

--- python/modulo 1/converter.py
+++ python/modulo 1/converter.py
@@ -6,3 +6,12 @@ print(preco)
 
 preco = int(preco)
 print(preco)
+
+idade = 20.5
+print(int(idade))
+
+idade_davi = "22"
+print(float(idade_davi))
+
+#as vezes ocorre erro de conversao
+#idade_ana = "vinte" não pode ser convertido para numero
