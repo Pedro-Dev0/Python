ab21d51f9810926959850c01aa8fbe897bb924a8
PedroSampaio <henripedro652@gmail.com>
2025-11-19 21:44:57 -0300

    Update converter.py

--- python/modulo 1/converter.py
+++ python/modulo 1/converter.py
@@ -13,5 +13,12 @@ print(int(idade))
 idade_davi = "22"
 print(float(idade_davi))
 
+print(type(str(20.5)))
+
 #as vezes ocorre erro de conversao
 #idade_ana = "vinte" não pode ser convertido para numero
+
+# a também é possível converter com / para divisão que resulta em float ou // para divisão inteira que resulta em int
+
+print(10 / 2)
+print(10 // 2)
