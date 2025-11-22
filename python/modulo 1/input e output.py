ebec4be8c005d4f7b569f63482a156cba6a084cd
PedroSampaio <henripedro652@gmail.com>
2025-11-22 11:24:54 -0300

    Update input e output.py

--- python/modulo 1/input e output.py
+++ python/modulo 1/input e output.py
@@ -2,3 +2,5 @@
 
 nome = input("Qual é o seu nome? ")
 print("Olá,", nome)
+
+# como viu acima , usamos a função input() para receber dados do usuário e a função print() para mostrar dados na tela, assim representando a entrada e a saída de dados.
