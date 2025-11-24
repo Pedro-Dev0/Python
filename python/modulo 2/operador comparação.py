1f61e9d819f007fd6778d6f9b4ec7231242940bc
PedroSampaio <henripedro652@gmail.com>
2025-11-24 20:28:42 -0300

    Create operador comparação.py

---
+++ python/modulo 2/operador comparação.py
@@ -0,0 +1,7 @@
+saldo = 200
+saque = 300
+
+print(saldo >= saque)  # Verifica se o saldo é maior ou igual ao saque
+print(saldo <= saque)  # Verifica se o saldo é menor ou igual ao saque
+print(saldo == saque)  # Verifica se o saldo é igual ao saque
+print(saldo != saque)  # Verifica se o saldo é diferente do saque
