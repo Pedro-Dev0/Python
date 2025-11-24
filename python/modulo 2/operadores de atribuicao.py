f5ed946d2354ff5a76eb2bc07981f3b21d67baa5
PedroSampaio <henripedro652@gmail.com>
2025-11-24 20:52:22 -0300

    Update operadores de atribuicao.py

---
+++ python/modulo 2/operadores de atribuicao.py
@@ -0,0 +1,30 @@
+saldo = 500
+
+print(f'Saldo inicial: {saldo}')
+
+saldo = saldo + 200 # atribuição comum
+print(f'Saldo após depósito: {saldo}')
+
+saldo += 200  # atribuição com operador de adição
+print(f'Saldo após segundo depósito: {saldo}')
+
+saldo -= 100  # atribuição com operador de subtração
+print(f'Saldo após saque: {saldo}')
+
+saldo *= 2  # atribuição com operador de multiplicação
+print(f'Saldo após investimento dobrado: {saldo}')
+
+saldo /= 2  # atribuição com operador de divisão
+print(f'Saldo após retirada do investimento: {saldo}')
+
+saldo //= 1  # atribuição com operador de divisão inteira
+print(f'Saldo após retirada inteira do investimento: {saldo}')
+
+saldo %= 300  # atribuição com operador de módulo
+print(f'Saldo após cálculo de módulo: {saldo}')
+
+saldo **= 2  # atribuição com operador de exponenciação
+print(f'Saldo após exponenciação: {saldo}')
+
+saldo = int(saldo)  # convertendo saldo para inteiro
+print(f'Saldo convertido para inteiro: {saldo}')
