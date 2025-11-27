37349786b52ea24bc5797248676ac994dfaeb8aa
PedroSampaio <henripedro652@gmail.com>
2025-11-26 22:19:08 -0300

    Create condicao.py

---
+++ python/modulo 3/condicao.py
@@ -0,0 +1,10 @@
+MAIOR_IDADE = 18
+idade = int(input("Digite sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Você é maior de idade, por isso é responsável pelos seus atos.")
+elif idade > 15 and idade < MAIOR_IDADE:
+    print("É menor de idade, mas já começou a entrar na adolescência.")
+else: #idade <= 15
+    print("Você é uma criança, aproveite sua infância!")
+
