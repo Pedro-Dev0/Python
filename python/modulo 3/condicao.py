67a136da9f50034f9cd624e3b1bc551520d75c9e
PedroSampaio <henripedro652@gmail.com>
2025-11-26 22:45:57 -0300

    Update condicao.py

--- python/modulo 3/condicao.py
+++ python/modulo 3/condicao.py
@@ -8,3 +8,10 @@ elif idade > 15 and idade < MAIOR_IDADE:
 else: #idade <= 15
     print("Você é uma criança, aproveite sua infância!")
 
+
+saldo = 500
+saque = 600
+#  ternario funciona como um if simplificado
+status = "Sucesso!" if saldo >= saque else "Falha!"
+
+print(f"{status} ao realizar o saque!")
