85fe838a09e479026c102034cda28e7ed6a03ac6
PedroSampaio <henripedro652@gmail.com>
2025-11-30 14:54:54 -0300

    Update estrutura de repetir.py

--- python/modulo 3/estrutura de repetir.py
+++ python/modulo 3/estrutura de repetir.py
@@ -4,5 +4,20 @@ VOGAIS = "AEIOU"
 for letra in texto:
     if letra.upper() in VOGAIS:
         print(letra, end="")
+else:
+    print()  # Para pular uma linha após a impressão das vogais
 
-print()  # Para pular uma linha após a impressão das vogais
+
+# built-in functions: range
+for numero in range(1, 12, 2):
+    print(numero, end="...")
+
+for numero in range(11):
+    print(numero, end=" ")
+
+tabuada = int(input("\nDigite um número para ver a tabuada: "))
+for i in range(1, 11):
+    resultado = tabuada * i
+    print(f"{tabuada} x {i} = {resultado}")
+
+# \n é o caractere de nova linha, usado para pular linhas
