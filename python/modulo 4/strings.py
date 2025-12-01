8cecb572ddefa126e3b676a64f75a9a39486e068
PedroSampaio <henripedro652@gmail.com>
2025-12-01 20:24:04 -0300

    Create strings.py

---
+++ python/modulo 4/strings.py
@@ -0,0 +1,13 @@
+curso = " pYtHon "
+
+print(curso.lower())  # Convierte a minúsculas
+print(curso.upper())  # Convierte a mayúsculas 
+print(curso.capitalize())  # Primera letra en mayúscula
+print(curso.title())  # Primera letra de cada palabra en mayúscula
+print(curso)
+print(curso.strip())  # Elimina espacios al inicio y al final
+print(curso.lstrip())  # Elimina espacios al inicio
+print(curso.rstrip())  # Elimina espacios al final
+
+print(curso.center(20, "*"))  # Centra el texto en un campo de 20 caracteres, rellenando con '*'
+print(".".join(curso)) # Une los caracteres con '.'
