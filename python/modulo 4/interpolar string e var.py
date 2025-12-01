64b0e64ba8076cf088070ea9c923b51d3b0acc74
PedroSampaio <henripedro652@gmail.com>
2025-12-01 20:49:21 -0300

    Update interpolar string e var.py

--- python/modulo 4/interpolar string e var.py
+++ python/modulo 4/interpolar string e var.py
@@ -6,3 +6,7 @@ profissao = "estágio em desenvolvimento"
 print("Meu nome é {}, tenho {} anos e no momento faço {} no cartório de notas." .format(nome, idade, profissao))
 
 print(f"Meu nome é {nome}, tenho {idade} anos e no momento atuo como {profissao} no cartório de notas.")
+
+PI = 3.14159
+print(f"O valor de PI com duas casas decimais é {PI:.2f}") #Duas casas decimais
+print(f"O valor de PI com duas casas decimais é {PI:10.2f}") #Espaço total de 10 caracteres, com 2 casas decimais
