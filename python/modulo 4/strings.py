33c71bc7355989bb1692ed7a45245c9a71eb28d6
PedroSampaio <henripedro652@gmail.com>
2025-12-01 20:29:28 -0300

    Update strings.py

--- python/modulo 4/strings.py
+++ python/modulo 4/strings.py
@@ -2,12 +2,15 @@ curso = " pYtHon "
 
 print(curso.lower())  # Convierte a minúsculas
 print(curso.upper())  # Convierte a mayúsculas 
-print(curso.capitalize())  # Primera letra en mayúscula
 print(curso.title())  # Primera letra de cada palabra en mayúscula
+
 print(curso)
+# imprime mas eliminando espaços 
 print(curso.strip())  # Elimina espacios al inicio y al final
 print(curso.lstrip())  # Elimina espacios al inicio
 print(curso.rstrip())  # Elimina espacios al final
+
+# centraliza o texto e coloca o caractere escolhido para preencher os espaços vazios e o join a cada caractere da string uni os com o caractere escolhido
 
 print(curso.center(20, "*"))  # Centra el texto en un campo de 20 caracteres, rellenando con '*'
 print(".".join(curso)) # Une cada carácter de la cadena con '.'
