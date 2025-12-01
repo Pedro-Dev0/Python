a5b319a8c5ff8906f6c56fbdc120c04f6683372a
PedroSampaio <henripedro652@gmail.com>
2025-12-01 20:45:24 -0300

    Create interpolar string e var.py

---
+++ python/modulo 4/interpolar string e var.py
@@ -0,0 +1,8 @@
+nome = "Pedro"
+idade = 30
+profissao = "estágio em desenvolvimento"
+
+
+print("Meu nome é {}, tenho {} anos e no momento faço {} no cartório de notas." .format(nome, idade, profissao))
+
+print(f"Meu nome é {nome}, tenho {idade} anos e no momento atuo como {profissao} no cartório de notas.")
