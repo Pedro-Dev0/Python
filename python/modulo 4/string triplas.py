d49b3fccb428ac543e976d5ed1fcf9194e89722c
PedroSampaio <henripedro652@gmail.com>
2025-12-01 21:24:17 -0300

    Create string triplas.py

---
+++ python/modulo 4/string triplas.py
@@ -0,0 +1,14 @@
+nome = "Pedro"
+
+mensagem = f"""
+Olá meu nome é {nome},
+venho de um reino distante,
+e vim te convidar para uma aventura!
+"""
+print(mensagem)
+
+print(f"""
+Olá meu nome é {nome},
+venho de um reino distante,
+e vim te convidar para uma aventura!
+""")
