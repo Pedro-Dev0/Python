e6c8a6c73aeae5a6378ad42cb0ae9d6f6f22651e
PedroSampaio <henripedro652@gmail.com>
2025-12-01 21:08:32 -0300

    Create fatiamento string.py

---
+++ python/modulo 4/fatiamento string.py
@@ -0,0 +1,17 @@
+nome = "Pedro Henrique Sampaio de Andrade"
+
+print(nome[0]) # P
+print(nome[1]) # e
+print(nome[2]) # d
+print(nome[3]) # r
+print(nome[4]) # o
+
+print(nome[:5]) # Pedro
+print(nome[6:15]) # Henrique
+print(nome[15:23]) # Sampaio
+print(nome[23:26]) # de
+print(nome[26:35]) # Andrade
+
+print(nome[:]) # Pedro Henrique Sampaio de Andrade
+print(nome[:: -1]) # ednardA ed oiapmaS eruinheH ordeP
+print(nome[:: 3]) # Pr hr eai eAdre
