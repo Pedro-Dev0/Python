77deb2ca4cab9e3fe3d63fec09986ab8bbd6db03
PedroSampaio <henripedro652@gmail.com>
2025-12-03 20:58:03 -0300

    Create listas.py

---
+++ python/modulo 5/listas.py
@@ -0,0 +1,8 @@
+frutas = ["manzana", "banana", "cereza", "naranja"] #lista de frutas, pode ser feitas com valores ou vazia
+
+
+letras = list("python") #cria uma lista a partir de uma string
+numeros = list(range(1, 6)) #cria uma lista a partir de um range de números
+print(frutas)
+print(letras)
+print(numeros)
