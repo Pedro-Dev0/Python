cb1ebb8b4d4c5348fb1435c85a3e5e2ac5dc3c0a
Pedro Sampaio <henripedro652@gmail.com>
2025-12-04 11:25:42 -0300

    Update listas.py

--- python/modulo 5/listas.py
+++ python/modulo 5/listas.py
@@ -1,5 +1,6 @@
 frutas = ["manzana", "banana", "cereza", "naranja"] #lista de frutas, pode ser feitas com valores ou vazia
 
+fruta_reserva = frutas.copy()
 
 letras = list("python") #cria uma lista a partir de uma string
 numeros = list(range(1, 6)) #cria uma lista a partir de um range de números
@@ -52,3 +53,5 @@ for carro in carros:
 for indice, carro in enumerate(carros):
     print(f"{indice}:{carro}")
     
+
+print(fruta_reserva)
