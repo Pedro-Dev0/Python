6c29fc87cfdf800c75aba6c4930dcde30606951a
PedroSampaio <henripedro652@gmail.com>
2025-12-03 21:49:39 -0300

    Create lista2.py

---
+++ python/modulo 5/lista2.py
@@ -0,0 +1,14 @@
+Mesas = list(range(30))
+print(Mesas)
+
+pares = []
+
+for numero in Mesas:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+print(pares) # primeiro filtro para se usar uma lista antiga e criar uma nova lista com base nessa antiga mas tirando as partes que não interessa ou não quer
+
+# usando list comprehension para fazer o mesmo que o código acima
+pares_comp = [numero for numero in Mesas if numero % 2 == 0]
+print(pares_comp)
