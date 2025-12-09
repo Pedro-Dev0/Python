7d16529508b30def5ed06a043441432d93dfb75f
Pedro Sampaio <henripedro652@gmail.com>
2025-12-09 10:11:13 -0300

    c

---
+++ python/modulo 6/metodos tupla.py
@@ -0,0 +1,5 @@
+.count para contar o que tem dentro
+
+# Exemplo:
+tupla = (1, 2, 2, 3, 2, 4)
+print(tupla.count(2))  # Output: 3
---
+++ python/modulo 6/tuplas.py
@@ -0,0 +1,23 @@
+tuplas = ("banana", "limão", "melancia",)
+print(tuplas)
+
+letras = tuple("Python")
+print(letras)
+
+numeros = tuple(range(10))
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
+
+print(tuplas[0])
+print(tuplas[-1])
+print(tuplas[-3])
+
+print(letras[2:])
+print(letras[1:])
+print(letras[:2])
+print(letras[0::2])
+print(letras[::])
+print(letras[::-1])
+print(letras[::-2])
