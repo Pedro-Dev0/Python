521c4c4e1b6d61b74e24fbab7959ebda36405435
PedroSampaio <henripedro652@gmail.com>
2025-12-27 11:58:20 -0300

    Update automação de banco.py

--- python/Primeiros modulos/Desafio 1/automação de banco.py
+++ python/Primeiros modulos/Desafio 1/automação de banco.py
@@ -1,3 +1,32 @@
+import textwrap
+
+def menu():
+    menu = """\n
+    ================ MENU ================
+    [d] \tDepositar
+    [s] \tSacar
+    [e] \tExtrato
+    [nc] \tNova Conta
+    [lc] \tListar Contas
+    [nu] \tNovo Usuário
+    [q] \tSair 
+    => """
+    return input(textwrap.dedent(menu))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 menu = """
 
 [d] Depositar
