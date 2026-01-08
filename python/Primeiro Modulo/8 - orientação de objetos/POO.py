a68483bb5ecf3dd2f26538df85a0ccc5e84efb8a
PedroSampaio <henripedro652@gmail.com>
2026-01-08 20:55:05 -0300

    Update POO.py

--- python/Primeiro Modulo/8 - orientação de objetos/POO.py
+++ python/Primeiro Modulo/8 - orientação de objetos/POO.py
@@ -14,9 +14,13 @@ class bicicleta:
     def correr(self):
         return "A bicicleta está correndo"
     
+    def sobre(self):
+        return f"Bicileta: Cor: {self.cor}, Modelo: {self.modelo}, Ano: {self.ano}, Valor: {self.valor}"
+    
 b2 = bicicleta("vermelha", "mountain bike", 2020, 1500)
 print(b2.buzinar())
 print(b2.parar())
 print(b2.correr())
+print(b2.sobre())
 
 b2.buzinar()
