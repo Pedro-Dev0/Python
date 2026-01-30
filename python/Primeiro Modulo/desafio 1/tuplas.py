695aa3020f6bb93e9f9bd52af1671374b38a1b0f
Pedro Sampaio <henripedro652@gmail.com>
2026-01-30 12:58:23 -0300

    Update tuplas.py

--- python/Primeiro Modulo/desafio 1/tuplas.py
+++ python/Primeiro Modulo/desafio 1/tuplas.py
@@ -14,17 +14,21 @@ if __name__ == "__main__":
 '''
 
 
-"""Desenvolva uma função que receba duas listas de números inteiros separados por espaço e retorne uma lista contendo apenas os elementos que são comuns a ambas as listas, sem duplicatas.
+'''Desenvolva uma função que receba duas listas de números inteiros separados por espaço e retorne uma lista contendo apenas os elementos que são comuns a ambas as listas, sem duplicatas.
 
 Detalhamento:
 
 Função elementos_comuns:
-1. Crie a função que converte cada elemento das listas lista1 e lista2 para inteiro usando map(int, lista) antes de convertê-los em conjuntos (set) e encontrar a interseção entre eles. Isso garante que tratemos os elementos corretamente como números inteiros e não como strings."""
+1. Crie a função que converte cada elemento das listas lista1 e lista2 para inteiro usando map(int, lista) antes de convertê-los em conjuntos (set) e encontrar a interseção entre eles. Isso garante que tratemos os elementos corretamente como números inteiros e não como strings.'''
+
+'''
+# TODO: Crie uma função 'elementos_comuns' que receba duas listas de números inteiros separados por espaço:
 
 def elementos_comuns(lista1, lista2):
-    set1 = set(lista1)
-    set2 = set(lista2)  
-    return list(set1.intersection(set2))
+    set1 = set(map(int, lista1))
+    set2 = set(map(int, lista2))
+    comuns = set1.intersection(set2)
+    return list(comuns)
 
 # Leitura das listas
 lista1 = input().split()
@@ -36,5 +40,26 @@ if all(item.isdigit() for item in lista1) and all(item.isdigit() for item in lis
     print(f"Elementos comuns às duas listas: {comuns}")
 else:
     print("Entrada inválida.")
+'''
 
+'''O desafio consiste em adicionar à função contar_caracteres um dicionário vazio chamado contador para armazenar as contagens de caracteres. Vamos iterar através de cada caractere na string string. Para cada caractere, verifique se ele já está presente no dicionário contador. Se estiver, incremente o valor associado a essa chave. Caso contrário, adicione a chave ao dicionário com valor inicial 1. Neste dicionário, as chaves são os caracteres presentes na string e os valores correspondem à contagem de vezes que cada caractere aparece.'''
+
+
+def contar_caracteres(string):
+#TODO: Inicialize um dicionário vazio 'contador' para armazenar as contagens de caracteres.:
+    contador = {}
+#TODO: Itere através de cada caractere na string string.
+    for char in string:
+#TODO: Para cada caractere, verifique se ele já está presente no dicionário contador:
+        if char in contador:
+            contador[char] += 1
+        else:
+            contador[char] = 1
+
+    return contador
+
+# Solicita entrada do usuário
+entrada = input()
+resultado = contar_caracteres(entrada)
+print(resultado)
     
