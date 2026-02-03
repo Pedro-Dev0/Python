98bebfe4629c2266e5637a9a6d1b5af0211a077f
PedroSampaio <henripedro652@gmail.com>
2026-02-02 21:24:20 -0300

    Update calculadora de soma.py

--- python/Primeiro Modulo/desafio 1/calculadora de soma.py
+++ python/Primeiro Modulo/desafio 1/calculadora de soma.py
@@ -1,17 +1,52 @@
-# TODO: Crie uma classe e método para realizar a soma:
-class Calculadora():
-  def soma(self):
-    num1 = int(input())
-    num2 = int(input())
-    return num1 + num2
+"""# TODO: Crie uma classe e método para realizar a soma:
+class Calculadora:
+    def soma(self, a, b):
+        return a + b
 
 num1 = int(input())
 num2 = int(input())
-
-
 
 # Criando uma instância da calculadora
 calc = Calculadora()
 
 resultado = calc.soma(num1, num2)
 print(resultado)
+
+"""
+
+
+"""class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+#TODO: Crie um método para retornar as informações formatas com Nome e Idade: 
+    def __str__(self):
+        return f"Nome: {self.nome}, Idade: {self.idade}"
+    
+
+# Entrada do usuário
+nome = input()
+idade = int(input())
+
+# TODO: Crie uma instância da pessoa:
+pessoa = Pessoa(nome, idade)
+
+#TODO: Chame o método para retornar as informações formatadas e imprima o resultado:
+print(pessoa)
+"""
+
+#TODO: Crie uma classe para converter temperaturas de Celsius para Fahrenheit e um método que realiza o cálculo de conversão:
+class ConversorTemperatura:
+    def celsius_para_fahrenheit(self, celsius):
+        return (celsius * 9/5) + 32
+
+# Entrada do usuário
+celsius = float(input())
+
+# TODO: Crie uma instância do conversor:
+
+conversor = ConversorTemperatura()
+
+fahrenheit = conversor.celsius_para_fahrenheit(celsius)
+print(fahrenheit)
