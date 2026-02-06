1052bf3011c2effd2c127245981f1c844b04e6c6
PedroSampaio <henripedro652@gmail.com>
2026-02-06 20:16:02 -0300

    Update decoradores_2.py

--- python/Primeiro Modulo/13 - decoradores/decoradores_2.py
+++ python/Primeiro Modulo/13 - decoradores/decoradores_2.py
@@ -1,12 +1,16 @@
 def meu_decorador(func):
     def wrapper(*args, **kwargs):
         print("Antes da função ser chamada.")
-        func(*args, **kwargs)  # Chama a função original
-        print("Depois da função ser chamada.")
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+    
     return wrapper
 
 @meu_decorador
 def minha_funcao(nome):
     print(f"Esta é a minha função, {nome}.")
+    
+    return nome.upper()
 
-minha_funcao("Alice")
+resultado = minha_funcao("Alice")
+print(f"Resultado da função decorada: {resultado}")
