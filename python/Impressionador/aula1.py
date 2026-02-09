c8ef04523046d7de241f3e9183ee73f63fb6dc0a
PedroSampaio <henripedro652@gmail.com>
2026-02-09 20:25:39 -0300

    ;..

---
+++ python/Impressionador/aula1.py
@@ -0,0 +1,11 @@
+
+import pyautogui
+
+#pyautogui.click -> clica com o mouse
+#pyautogui.write -> escreve um texto
+#pyautogui.press aperta uma tecla
+#pyautogui.hotkey -> aperta um atalho
+
+pyautogui.press('win')
+
+
