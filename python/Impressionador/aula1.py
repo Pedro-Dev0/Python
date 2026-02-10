a91babb654e5bc8b40299bbccc568a1b12657cfa
PedroSampaio <henripedro652@gmail.com>
2026-02-09 21:48:50 -0300

    Update aula1.py

--- python/Impressionador/aula1.py
+++ python/Impressionador/aula1.py
@@ -1,11 +1,30 @@
 
+import time
 import pyautogui
 
 #pyautogui.click -> clica com o mouse
 #pyautogui.write -> escreve um texto
 #pyautogui.press aperta uma tecla
 #pyautogui.hotkey -> aperta um atalho
+def pausa(tempo):
+    time.sleep(tempo)
 
+def abrir_programa(nome_programa):
+    pyautogui.press('win')
+    pausa(2)
+    pyautogui.write(nome_programa)
+    pausa(1)
+    pyautogui.press('enter')
+
+programa = input("Qual programa deseja abrir? ")
+input(f"Pressione Enter para abrir {programa}...")
+
+abrir_programa(programa)
+
+
+"""pausa(3)
 pyautogui.press('win')
-
-
+pausa(2)
+pyautogui.write('bloco de notas')
+pausa(1)
+pyautogui.press('enter')"""
