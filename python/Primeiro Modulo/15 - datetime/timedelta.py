4ff60d3d35e9d1d6dc0bba863a2794e36965a303
Pedro Sampaio <henripedro652@gmail.com>
2026-02-20 15:03:27 -0300

    Update timedelta.py

--- python/Primeiro Modulo/15 - datetime/timedelta.py
+++ python/Primeiro Modulo/15 - datetime/timedelta.py
@@ -2,15 +2,19 @@
 
 from datetime import datetime, time, date, timedelta
 
-tipo_carro = 'G'
+tipo_carro = 'P'
 tempo_pequeno = 30
 tempo_medio = 45
 tempo_grande = 60
 data_atual = datetime.now()
 
+# formatar para data brasileira
+#data_atual = data_atual.strftime('%d/%m/%Y %H:%M:%S')
+
 if tipo_carro == 'P':
-    data_estimada = data_atual + timedelta(minutes=tempo_pequeno)
-    print(f'O carro saiu ás {data_atual} e a previsão de chegada é ás {data_estimada}')
+    data_estimada = data_atual + timedelta.strftime('%d/%m/%Y %H:%M:%S')(minutes=tempo_pequeno)
+    data_estimada_formatada = data_estimada.strftime('%d/%m/%Y %H:%M:%S')
+    print(f'O carro saiu ás {data_atual} e a previsão de chegada é ás {data_estimada_formatada}')
 elif tipo_carro == 'M':
     data_estimada = data_atual + timedelta(minutes=tempo_medio)
     print(f'O carro saiu ás {data_atual} e a previsão de chegada é ás {data_estimada}')
