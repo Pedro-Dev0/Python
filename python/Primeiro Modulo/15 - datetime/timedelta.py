4033112f98c655be547bba241b266609bdea009b
Pedro Sampaio <henripedro652@gmail.com>
2026-02-20 16:05:53 -0300

    Update timedelta.py

--- python/Primeiro Modulo/15 - datetime/timedelta.py
+++ python/Primeiro Modulo/15 - datetime/timedelta.py
@@ -8,16 +8,22 @@ tempo_medio = 45
 tempo_grande = 60
 data_atual = datetime.now()
 
-# formatar para data brasileira
-#data_atual = data_atual.strftime('%d/%m/%Y %H:%M:%S')
 
 if tipo_carro == 'P':
-    data_estimada = data_atual + timedelta.strftime('%d/%m/%Y %H:%M:%S')(minutes=tempo_pequeno)
+    data_estimada = data_atual + timedelta(minutes=tempo_pequeno)
     data_estimada_formatada = data_estimada.strftime('%d/%m/%Y %H:%M:%S')
     print(f'O carro saiu ás {data_atual} e a previsão de chegada é ás {data_estimada_formatada}')
 elif tipo_carro == 'M':
     data_estimada = data_atual + timedelta(minutes=tempo_medio)
-    print(f'O carro saiu ás {data_atual} e a previsão de chegada é ás {data_estimada}')
+    data_estimada_formatada = data_estimada.strftime('%d/%m/%Y %H:%M:%S')
+    print(f'O carro saiu ás {data_atual} e a previsão de chegada é ás {data_estimada_formatada}')
 else:
     data_estimada = data_atual + timedelta(minutes=tempo_grande)
-    print(f'O carro saiu ás {data_atual} e a previsão de chegada é ás {data_estimada}')
+    data_estimada_formatada = data_estimada.strftime('%d/%m/%Y %H:%M:%S')
+    print(f'O carro saiu ás {data_atual} e a previsão de chegada é ás {data_estimada_formatada}')
+
+
+strptime = datetime.strptime('20/06/2024 15:30:00', '%d/%m/%Y %H:%M:%S')
+print(strptime)
+strftime = strptime.strftime('%d/%m/%Y %H:%M:%S')
+print(strftime)
