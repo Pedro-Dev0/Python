2ef13daaab414bb02d2f4b2007b72a92207e3720
Pedro Sampaio <henripedro652@gmail.com>
2026-03-04 15:36:29 -0300

    c

---
+++ python/Primeiro Modulo/16 - arquivos/escrever arquivo.py
@@ -0,0 +1,5 @@
+# escrever um arquivo
+file = open('C:\\Users\\suporte\\Documents\\GitHub\\Python\\python\\Primeiro Modulo\\16 - arquivos\\texto.txt', 'w')
+file.write('Valorant é um jogo de tiro em primeira pessoa desenvolvido pela Riot Games.\n')                                                         
+file.close()
+
