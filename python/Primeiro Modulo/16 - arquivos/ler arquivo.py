45114c190086e282491baa2315113e6d49e7a630
Pedro Sampaio <henripedro652@gmail.com>
2026-03-04 15:31:26 -0300

    Update ler arquivo.py

--- python/Primeiro Modulo/16 - arquivos/ler arquivo.py
+++ python/Primeiro Modulo/16 - arquivos/ler arquivo.py
@@ -3,3 +3,8 @@ file = open('C:\\Users\\Malleus\\Documents\\valorant.txt', 'r')
 #print(file.readline()) # só a primeira linha do mesmo
 print(file.readlines()) # aparece divido por espaços estranhos
 file.close()
+
+# r = read - ler
+# w = write - escrever
+# a = append - acrescentar
+
