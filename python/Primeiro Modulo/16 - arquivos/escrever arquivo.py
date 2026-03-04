ba73735dd218d255061b78dc1d0294dcc7779fe4
Pedro Sampaio <henripedro652@gmail.com>
2026-03-04 15:39:09 -0300

    x

--- python/Primeiro Modulo/16 - arquivos/escrever arquivo.py
+++ python/Primeiro Modulo/16 - arquivos/escrever arquivo.py
@@ -1,5 +1,5 @@
 # escrever um arquivo
 file = open('C:\\Users\\suporte\\Documents\\GitHub\\Python\\python\\Primeiro Modulo\\16 - arquivos\\texto.txt', 'w')
-file.write('Valorant é um jogo de tiro em primeira pessoa desenvolvido pela Riot Games.\n')                                                         
+file.write(f'BERSERK é um anime e manga feito por Kentaro Miura e Tetsuya Saito e fala sobre a luta pela sobrevivência em um mundo pós-apocalíptico, tudo isso interpretado por seu protagonista o Guts que se depara com inúmeras adversidades e desafios juntamente com a marca da maldição.\n')                                                         
 file.close()
 
