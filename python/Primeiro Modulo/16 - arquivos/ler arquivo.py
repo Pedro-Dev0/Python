7152fc41cfb07449332cb40f89bd521644d9977c
PedroSampaio <henripedro652@gmail.com>
2026-03-03 21:00:16 -0300

    Create ler arquivo.py

---
+++ python/Primeiro Modulo/16 - arquivos/ler arquivo.py
@@ -0,0 +1,5 @@
+file = open('C:\\Users\\Malleus\\Documents\\valorant.txt', 'r')
+#print(file.read()) # todo arquivo
+#print(file.readline()) # só a primeira linha do mesmo
+print(file.readlines()) # aparece divido por espaços estranhos
+file.close()
