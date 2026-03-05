f6cefb50e6ab5339e2de30f07aa847f2df05a270
PedroSampaio <henripedro652@gmail.com>
2026-03-04 21:24:52 -0300

    dos

--- python/Primeiro Modulo/16 - arquivos/gerenciar.py
+++ python/Primeiro Modulo/16 - arquivos/gerenciar.py
@@ -5,11 +5,21 @@ import shutil
 #os.mkdir("exemplo")
 
 #mover arquivo
-shutil.move('C:/Users/Malleus/Documents/GitHub/Python/gurikakuri.txt' , 'C:/Users/Malleus/Documents/GitHub/Python/python/Primeiro Modulo/16 - arquivos')
+#shutil.move('C:/Users/Malleus/Documents/GitHub/Python/girikakuri.txt' , 'C:/Users/Malleus/Documents/GitHub/Python/python/Primeiro Modulo/16 - arquivos')
 
 #renomear arquivo
-os.rename('C:/Users/Malleus/Documents/GitHub/Python/python/Primeiro Modulo/16 - arquivos/gurikakuri.txt' , 'bbnomoney.txt')
+#os.rename('C:/Users/Malleus/Documents/GitHub/Python/python/Primeiro Modulo/16 - arquivos/girikakuri.txt' , 'C:/Users/Malleus/Documents/GitHub/Python/python/Primeiro Modulo/16 - arquivos/bbnomoney.txt')
+
+#escrever
+file = open('C:/Users/Malleus/Documents/GitHub/Python/python/Primeiro Modulo/16 - arquivos/bbnomoney.txt', 'w+')
+file.write(f'BERSERK é um anime e manga feito por Kentaro Miura e Tetsuya Saito e fala sobre a luta pela sobrevivência em um mundo pós-apocalíptico, tudo isso interpretado por seu protagonista o Guts que se depara com inúmeras adversidades e desafios juntamente com a marca da maldição\n AHHAAHAHAHAH')
+
+#ler
+file = open('C:/Users/Malleus/Documents/GitHub/Python/python/Primeiro Modulo/16 - arquivos/bbnomoney.txt', 'r')
+conteudo = file.read()
+print(conteudo)
+file.close
 
 #remover arquivo
-os.remove('C:/Users/Malleus/Documents/GitHub/Python/bbnomoney.txt')
+#os.remove('C:/Users/Malleus/Documents/GitHub/Python/python/Primeiro Modulo/16 - arquivos/bbnomoney.txt')
 
