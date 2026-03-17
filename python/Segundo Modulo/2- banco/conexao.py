fcbc113cd5d6387e316717da2e71a1c07c708de2
PedroSampaio <henripedro652@gmail.com>
2026-03-17 20:57:11 -0300

    banco em python

--- python/Segundo Modulo/2- banco/conexao.py
+++ python/Segundo Modulo/2- banco/conexao.py
@@ -7,26 +7,35 @@ conexao = sqlite3.connect(ROOT_PATH / "clientes.db")
 cursor = conexao.cursor()
 
 # Cria tabela (rode uma vez, depois comente)
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS clientes (
+def criar_tabela(conexao, cursor):
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS clientes (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nome VARCHAR(100),
         email VARCHAR(100)
-    )
-""")
+        )
+    """)
+    conexao.commit()
+    print("✅ Tabela criada!")
 
 # Insere dados
-data = ("Murilo", "Murilao@123.com")
-cursor.execute(
-    "INSERT OR REPLACE INTO clientes (nome, email) VALUES (?, ?)",
+def inserir_registro(conexao, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute(
+    "INSERT OR IGNORE INTO clientes (nome, email) VALUES (?, ?)",
     data
-)
+    )
+    conexao.commit()
+    print("✅ Dados inseridos!")
 
-update = ("Murilox", 2)
-cursor.execute(
-    "UPDATE clientes SET nome = ? WHERE id = ?", update
-)
+def atualizar_registro(conexao, cursor, nome, email, id):
+    update = (nome, email, id)
+    cursor.execute(
+        "UPDATE clientes SET nome=?, email=? WHERE id=?", update
+    )
+    conexao.commit()
+    conexao.close()
+    print("✅ Dados atualizados!")
 
-conexao.commit()
-conexao.close()
-print("✅ Dados inseridos!")
+atualizar_registro(conexao, cursor, "GUI", "GUIMESs@gmail.com", 5)
