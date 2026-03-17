2cbe26131267abf9d3ce798c888bad70be0bc0b8
Pedro Sampaio <henripedro652@gmail.com>
2026-03-17 15:10:20 -0300

    Update conexao.py

--- python/Segundo Modulo/2- banco/conexao.py
+++ python/Segundo Modulo/2- banco/conexao.py
@@ -4,7 +4,9 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 
 conexao = sqlite3.connect(ROOT_PATH / "clientes.db")
-#print(conexao) para criar banco de dados
+# print(conexao) para criar banco de dados
 
 cursor = conexao.cursor()
-cursor.execute("CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(100))")
+cursor.execute(
+    "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(100))"
+)
