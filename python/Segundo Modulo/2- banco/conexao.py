9229a8134cb08ae8667c551a511cc236d990a73c
PedroSampaio <henripedro652@gmail.com>
2026-03-16 21:08:50 -0300

    DB

---
+++ python/Segundo Modulo/2- banco/conexao.py
@@ -0,0 +1,10 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
+conexao = sqlite3.connect(ROOT_PATH / "clientes.db")
+#print(conexao) para criar banco de dados
+
+cursor = conexao.cursor()
+cursor.execute("CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(100))")
