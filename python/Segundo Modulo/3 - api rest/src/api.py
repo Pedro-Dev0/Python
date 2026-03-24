d7083cec4657aa6ca9c6ebfe4eec18d55234e26f
PedroSampaio <henripedro652@gmail.com>
2026-03-24 20:25:59 -0300

    Update api.py

--- python/Segundo Modulo/3 - api rest/src/api.py
+++ python/Segundo Modulo/3 - api rest/src/api.py
@@ -10,12 +10,16 @@ def hello_world(usuario, idade, altura):
     print(type(altura))
     return f"<p>Hello World! {usuario.upper()}, idade:{idade}</p>"
 
-@app.route("/bemvindo")
+@app.route("/bemvindo", methods=["GET", "POST"])
 def bem_vindo():
-    return "<p>Bem vindo!</p>"
+    if request.method == "GET":
+        return 'É get'
+    else:
+        return 'We Post'
+
 
 #na rota com ("/bemvindo/") vai redirecionar para a pagina correta, sem / no final ai a pagina é unica e se for colocada da erro!
 
 with app.test_request_context():
-    print(url_for('olamundo'))
-    print(url_for('bemvindo'))
+    print(url_for('hello_world', usuario='pedro', idade=23, altura=1.89))
+    print(url_for('bem_vindo'))
