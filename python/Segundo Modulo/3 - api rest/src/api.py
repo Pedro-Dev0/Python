d02ee45560f8e9992e01f381ffa5a884421761ed
PedroSampaio <henripedro652@gmail.com>
2026-03-24 20:06:22 -0300

    Update api.py

--- python/Segundo Modulo/3 - api rest/src/api.py
+++ python/Segundo Modulo/3 - api rest/src/api.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World!</p>"
+@app.route("/olamundo/<usuario>/<int:idade>/<float:altura>")
+def hello_world(usuario, idade, altura):
+    print(idade)
+    print(type(idade))
+    print(altura)
+    print(type(altura))
+    return f"<p>Hello World! {usuario.upper()}, idade:{idade}</p>"
 
 @app.route("/bemvindo")
 def bem_vindo():
     return "<p>Bem vindo!</p>"
+
+#na rota com ("/bemvindo/") vai redirecionar para a pagina correta, sem / no final ai a pagina é unica e se for colocada da erro!
+
+with app.test_request_context():
+    print(url_for('olamundo'))
+    print(url_for('bemvindo'))
