bd113c20da8969850c897b340fecf44993ab6091
PedroSampaio <henripedro652@gmail.com>
2026-04-02 23:18:59 -0300

    api

--- python/Segundo Modulo/3 - api rest/src/controllers/user.py
+++ python/Segundo Modulo/3 - api rest/src/controllers/user.py
@@ -37,3 +37,11 @@ def handle_user():
     else:
         return {'users': [_list_users()]}
     
+@app.route('/<int:user_id>')
+def get_user(user_id):
+    user = db.get_or_404(User, user_id)
+    return {
+        "id": user_id,
+        "username": user.username,
+    }
+    
