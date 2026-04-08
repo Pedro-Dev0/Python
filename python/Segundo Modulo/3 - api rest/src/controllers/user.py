20e5deaf4a0bab645e290c1d441700d7414d5018
PedroSampaio <henripedro652@gmail.com>
2026-04-07 21:43:47 -0300

    db

--- python/Segundo Modulo/3 - api rest/src/controllers/user.py
+++ python/Segundo Modulo/3 - api rest/src/controllers/user.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request
 from src.app import User, db
 from http import HTTPStatus
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy import inspect
+
 
 app = Blueprint('user', __name__, url_prefix='/users')
 
@@ -49,25 +51,28 @@ def get_user(user_id):
 
 @app.route('/<int:user_id>', methods=['PATCH', 'PUT'])
 def update_user(user_id):
-    # 1. Busca o usuário ou retorna 404 (Estilo do Professor)
     user = db.get_or_404(User, user_id)
-    data = request.json
+    data = request.json #
 
-    # 2. Lógica Dinâmica: Definimos quais campos PODEM ser editados
-    attrs = ['username', 'email']
-    # 3. O "Pulo do Gato": Percorremos a lista e atualizamos apenas o que foi enviado
-    for attr in attrs:
-        if attr in data:
-            setattr(user, attr, data[attr])
+    # Se o 'data' for None, significa que o corpo da requisição veio vazio
+    if not data:
+        return {"error": "Corpo da requisição não pode estar vazio"}, 400
+
+    mapper = inspect(User) # Técnica da imagem do professor
+    for column in mapper.attrs:
+        if column.key in data:
+            setattr(user, column.key, data[column.key])
     
-    try:
-        db.session.commit()
-    except IntegrityError:
-        db.session.rollback()
-        return {"error": "Username já existe"}, HTTPStatus.CONFLICT
-
+    db.session.commit() #
     return {
         "id": user.id,
         "username": user.username,
         "email": user.email
     }
+
+@app.route('/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = db.get_or_404(User, user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return '', HTTPStatus.NO_CONTENT
