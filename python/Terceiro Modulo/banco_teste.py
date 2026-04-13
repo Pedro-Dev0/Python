ab458e9982b311ebc70e296e22a575e0f27c1f70
PedroSampaio <henripedro652@gmail.com>
2026-04-13 20:34:06 -0300

    Create banco_teste.py

---
+++ python/Terceiro Modulo/banco_teste.py
@@ -0,0 +1,83 @@
+#começando com criação da base do banco o cliente 
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas=[]
+
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+
+    self._historico = Historico()
+
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    def sacar(self, valor):
+        saldo = self._saldo
+        excedeu_saldo = valor > saldo
+
+        if excedeu_saldo:
+            print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+            return False
+
+
+        elif valor > 0:
+            self._saldo -= valor
+            print("\n=== Saque realizado com sucesso! ===")
+            return True
+        
+        else:
+            print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+            return False
+        
+
+
+        
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
